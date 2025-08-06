e130ef26f2a182fd94b834fa913188b91ceb1886
govardhan <govardhanreddydasari07>
2025-08-06 16:29:35 +0530

    hello

--- Hello.c
+++ Hello.c
@@ -1,11 +1,16 @@
 
 #include <stdio.h>
-
+hello world
 
 int main() {
 
     
     printf("Hello World");
+    printf("govardhan reddy");
+    int i =0;
+    int j=15;
+    int k=i+j;
+    printf(%d k);
 
     return 0;
 }
